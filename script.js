d11c4a11d7f619790d79fdb4bb565516075f47ab
Sourabh Upadhyay <usourabh99@gmail.com>
2024-02-26 18:03:20 +0530

    added dropdown designing

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const loadPlacesinDropdown = () => {
+const loadPlacesinDropdown = (e) => {
   try {
     const citySelector = document.querySelector("#selectHeader");
     const lowerCaseText = citySelector.value.toLowerCase();
 
     if (lowerCaseText == "city") {
+      // console.log("hello");
       fetch("db.json")
         .then((response) => {
           return response.json();
         })
         .then((singleData) => {
-          const allCities = Object.keys(singleData);
-          allCities.forEach((place) => {
-            console.log();
-          });
+          console.log(typeof singleData);
+          // const a = Object.keys(singleData);
+          // console.log(a);
         })
         .catch((err) => {
           console.error(err);
